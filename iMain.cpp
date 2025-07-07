d6bca426be16076a89e28494e4ffca304ae92b48
Himel Chowdhury <iamhimelchy@gmail.com>
2025-07-07 23:04:58 +0600

    Update iMain.cpp

--- iMain.cpp
+++ iMain.cpp
@@ -1,200 +1,268 @@
 #include "iGraphics.h"
 #include "iSound.h"
-#include <time.h> 
-#include <stdlib.h> 
-double x1=0,y11=0; 
-char hab[10][13]={"hab001_1.png","hab002_1.png","hab003_1.png","hab004_1.png","hab005_1.png","hab006_1.png","hab007_1.png","hab008_1.png","hab009_1.png","hab010_1.png"};
-char obs[3][10]={"obs1.png","obs2.png","obs3.png"};
-char cloud[7][13]={"cloud1.png","cloud2.png","cloud3.png","cloud4.png","cloud5.png","cloud6.png","cloud7.png"};
-char menu[5][15]={"menu_1.png","menu_2.png","menu_3.png","menu_4.png","menu_5.png"};
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <ctype.h>
+
+using namespace std;
+
+struct PlayerScore {
+    string name;
+    int score;
+    bool operator<(const PlayerScore& other) const {
+        return score > other.score;
+    }
+};
+
+vector<PlayerScore> highScores;
+const string HIGHSCORE_FILE = "highscores.txt";
+const int MAX_HIGHSCORES = 5;
+
+char playerName[50] = "";
+int playerNameIndex = 0;
+bool inputName = false;
+int finalScore = 0;
+float obstacleSpeed = 2.0;
+int health = 3;
+
+char hab[10][13] = {"hab001_1.png","hab002_1.png","hab003_1.png","hab004_1.png","hab005_1.png",
+                    "hab006_1.png","hab007_1.png","hab008_1.png","hab009_1.png","hab010_1.png"};
+char obs[3][10] = {"obs1.png","obs2.png","obs3.png"};
+char cloud[7][13] = {"cloud1.png","cloud2.png","cloud3.png","cloud4.png","cloud5.png","cloud6.png","cloud7.png"};
+char menu[5][15] = {"menu_1.png","menu_2.png","menu_3.png","menu_4.png","menu_5.png"};
 char menuBig[5][20] = {"menu_1_big.png","menu_2_big.png","menu_3_big.png","menu_4_big.png","menu_5_big.png"};
+
 bool hoverMenu[5] = {false};
 int menuX[5] = {100, 300, 20, 185, 350};
 int menuY[5] = {300, 300, 50, 50, 50};
 int menuW = 120, menuH = 160;
-Image menuBigImg[5];
-Image cloudImg[7];
-Image skyImg;
-Image heartbreak;
-Image menuImg[5];
-Image menuImghab;
+
+Image menuBigImg[5], cloudImg[7], skyImg, heartbreak, menuImg[5], menuImghab, back;
+Image habimgfrm[10], obsimgfrm[3];
+Sprite habimg[10], obsimg[3];
+
+int gamestate = 0;
+int score = 0, scoretick = 0;
+int bi = 0;
+int obsx[3], obsy[3], obsi[3];
+int x = 200, y = 70;
+int easy;
+bool isObsActive[3] = {true, true, true};
 bool showHeartbreak = false;
 int heartbreakCounter = 0;
-Sprite habimg[10];
-Sprite obsimg[3];
-Image habimgfrm[10];
-Image obsimgfrm[3];
-int gamestate=0;
-int score=0,scoretick=0;
-int bi=0;
-int obsx[3],obsy[3],obsi[3];
-int x=200,y=70;
-int easy,wrong;
-void scoreupdate()
-{
-    scoretick++;
-    if(scoretick%9==0)
-      score++;
-}
-void move()
-{
-    if(isSpecialKeyPressed(GLUT_KEY_RIGHT))x+=15;
-    if(isSpecialKeyPressed(GLUT_KEY_LEFT)) x-=15;
-    if(x > 460) x = -40;
-    if(x < -40) x = 460;
-    iSetSpritePosition(&habimg[bi], x, y);
-}
 
 double c1x=100,c2x=150,c3x=200,c4x=400,c5x=250,c6x=320,c7x=425;
 double c1y=114,c2y=229,c3y=343,c4y=457,c5y=500,c6y=686,c7y=800;
-double yy=800;
 
-bool isObsActive[3] = {true, true, true};
-void obstacle()
-{
-    for(int i=0;i<3;i++)
-    {
-        if(isObsActive[i]){
-            obsy[i]-=2;
-        if(obsy[i]<-50)
-        {
-                obsy[i]=800;
-                obsx[i]=rand()%300;
-                obsi[i]=rand()%3;
-                iChangeSpriteFrames(&obsimg[i], &obsimgfrm[obsi[i]], 1);   
-        }
-        iSetSpritePosition(&obsimg[i], obsx[i], obsy[i]);
+void scoreupdate() {
+    scoretick++;
+    if(scoretick % 9 == 0) {
+        score++;
+        obstacleSpeed = 2.0 + (score * 0.025);
     }
-    else{
-          obsy[i] -= 2;
+}
 
-        if (obsy[i] < -50)
-            {
-                isObsActive[i] = true; // Reactivate it!
+void loadHighScores() {
+    highScores.clear();
+    ifstream file(HIGHSCORE_FILE);
+    if (file.is_open()) {
+        string name; int score;
+        while (file >> name >> score) {
+            highScores.push_back({name, score});
+        }
+        file.close();
+        sort(highScores.begin(), highScores.end());
+    }
+}
 
+void saveHighScores() {
+    ofstream file(HIGHSCORE_FILE);
+    for (int i = 0; i < min((int)highScores.size(), MAX_HIGHSCORES); ++i) {
+        file << highScores[i].name << " " << highScores[i].score << endl;
+    }
+    file.close();
+}
+
+void addHighScore(string name, int score) {
+    for (char &c : name) if (c == ' ') c = '_';
+    highScores.push_back({name, score});
+    sort(highScores.begin(), highScores.end());
+    if (highScores.size() > MAX_HIGHSCORES)
+        highScores.resize(MAX_HIGHSCORES);
+    saveHighScores();
+    loadHighScores(); 
+}
+
+void move() {
+    if (isSpecialKeyPressed(GLUT_KEY_RIGHT)) x += 15;
+    if (isSpecialKeyPressed(GLUT_KEY_LEFT)) x -= 15;
+    if (x > 460) x = -40;
+    if (x < -40) x = 460;
+    iSetSpritePosition(&habimg[bi], x, y);
+}
+
+void obstacle() {
+    for (int i = 0; i < 3; i++) {
+        if (isObsActive[i]) {
+            obsy[i] -= obstacleSpeed;
+            if (obsy[i] < -50) {
                 obsy[i] = 800;
                 obsx[i] = rand() % 300;
                 obsi[i] = rand() % 3;
                 iChangeSpriteFrames(&obsimg[i], &obsimgfrm[obsi[i]], 1);
-               isObsActive[i]=true;
             }
-                        iSetSpritePosition(&obsimg[i], obsx[i], obsy[i]);
-    }
+            iSetSpritePosition(&obsimg[i], obsx[i], obsy[i]);
+        } else {
+            obsy[i] -= obstacleSpeed;
+            if (obsy[i] < -50) {
+                isObsActive[i] = true;
+                obsy[i] = 800;
+                obsx[i] = rand() % 300;
+                obsi[i] = rand() % 3;
+                iChangeSpriteFrames(&obsimg[i], &obsimgfrm[obsi[i]], 1);
+            }
+            iSetSpritePosition(&obsimg[i], obsx[i], obsy[i]);
+        }
     }
 }
-/*bool collision(int x1, int y1,int h1,int w1, int x2, int y2,int h2, int w2)
-{
-    return (x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2);
-}*/
 
-    
-void incmnt()
-{
-    bi++;
-    if(bi==10)
-     bi=0;
-     iChangeSpriteFrames(&habimg[bi], &habimgfrm[bi], 1);
-     iSetSpritePosition(&habimg[bi], x, y);
+void incmnt() {
+    bi = (bi + 1) % 10;
+    iChangeSpriteFrames(&habimg[bi], &habimgfrm[bi], 1);
+    iSetSpritePosition(&habimg[bi], x, y);
 }
-void cldx()
-{
-    c1x+=.2;
-    if(c1x>490)c1x=-150;
-    c2x-=.1;
-    if(c2x<-100)c2x=550;
-    c3x-=.3;
-    if(c3x<-100)c3x=550;
-    c4x+=.5;
-    if(c4x>490)c4x=-150;
-    c5x+=.25;
-    if(c5x>490)c5x=-150;
-    c6x-=.3;
-    if(c6x<-100)c6x=550;
-    c7x+=.8;
-    if(c7x>490)c7x=-150;
+
+void cldx() {
+    double* cx[] = {&c1x, &c2x, &c3x, &c4x, &c5x, &c6x, &c7x};
+    double speed[] = {.2, -.1, -.3, .5, .25, -.3, .8};
+    for (int i = 0; i < 7; i++) {
+        *cx[i] += speed[i];
+               if (speed[i] > 0 && *cx[i] > 550) {     // Moving right
+            *cx[i] = -150;
+        }
+        else if (speed[i] < 0 && *cx[i] < -150) { // Moving left
+            *cx[i] = 550;
+        }
+    }
 }
-void cldy()
-{
-    c1y-=2;
-    if(c1y<-50)c1y=800;
-    c2y-=2;
-    if(c2y<-50)c2y=800;
-    c3y-=2;
-    if(c3y<-50)c3y=800;
-    c4y-=2;
-    if(c4y<-50)c4y=800; 
-    c5y-=2;
-    if(c5y<-50)c5y=800;
-    c6y-=2;
-    if(c6y<-50)c6y=800;
-    c7y-=2;
-    if(c7y<-50)c7y=800; 
+
+void cldy() {
+    double* cy[] = {&c1y, &c2y, &c3y, &c4y, &c5y, &c6y, &c7y};
+    for (int i = 0; i < 7; i++) {
+        *cy[i] -= 2;
+        if (*cy[i] < -50) *cy[i] = 800;
+    }
 }
-void iDraw()
-{
+
+void iDraw() {
     iClear();
-   
-    
-    iShowLoadedImage(0,0,&skyImg);
-    iShowLoadedImage(c1x,c1y,&cloudImg[0]);
-    iShowLoadedImage(c2x,c2y,&cloudImg[1]);
-    iShowLoadedImage(c3x,c3y,&cloudImg[2]);
-    iShowLoadedImage(c4x,c4y,&cloudImg[3]);
-    iShowLoadedImage(c5x,c5y,&cloudImg[4]);
-    iShowLoadedImage(c6x,c6y,&cloudImg[5]);
-    iShowLoadedImage(c7x,c7y,&cloudImg[6]);
-     if(gamestate==0)
-    {
-      iShowLoadedImage(50, 530, &menuImghab);
 
-    for (int i = 0; i < 5; i++)
-    {
-        if (hoverMenu[i])
-        {
-            // Show big image (assume it's already pre-sized)
-            iShowLoadedImage(menuX[i] - 10, menuY[i] - 10, &menuBigImg[i]);
-        }
-        else
-        {
-            // Show normal image
-            iShowLoadedImage(menuX[i], menuY[i], &menuImg[i]);
+    // Draw background and clouds
+    iShowLoadedImage(0, 0, &skyImg);
+    iShowLoadedImage(c1x, c1y, &cloudImg[0]);
+    iShowLoadedImage(c2x, c2y, &cloudImg[1]);
+    iShowLoadedImage(c3x, c3y, &cloudImg[2]);
+    iShowLoadedImage(c4x, c4y, &cloudImg[3]);
+    iShowLoadedImage(c5x, c5y, &cloudImg[4]);
+    iShowLoadedImage(c6x, c6y, &cloudImg[5]);
+    iShowLoadedImage(c7x, c7y, &cloudImg[6]);
+
+    if (gamestate == 0) {
+        // Main menu
+        iShowLoadedImage(50, 530, &menuImghab);
+        for (int i = 0; i < 5; i++) {
+            if (hoverMenu[i]) 
+                iShowLoadedImage(menuX[i] - 10, menuY[i] - 10, &menuBigImg[i]);
+            else 
+                iShowLoadedImage(menuX[i], menuY[i], &menuImg[i]);
         }
     }
-    }
+    else if (gamestate == 1) {
+        // Gameplay
+        iShowSprite(&habimg[bi]);
+        for (int i = 0; i < 3; i++) {
+            if (isObsActive[i]) {
+                iShowSprite(&obsimg[i]);
+                if (iCheckCollision(&habimg[bi], &obsimg[i])) {
+                    showHeartbreak = true;
+                    heartbreakCounter = 60;
+                    iPauseSound(easy);
+                    iPlaySound("wrong.wav", false, 80);
+                    health--;
+                    isObsActive[i] = false;
 
-    if(gamestate==1){
-      iShowSprite(&habimg[bi]);
-  for(int i=0;i<3;i++)
-  {
-    if(isObsActive[i])
-    {
-    iShowSprite(&obsimg[i]);
-    if(iCheckCollision(&habimg[bi], &obsimg[i]))
-    {
-         showHeartbreak = true;
-    heartbreakCounter = 60;
-        iPauseSound(easy);
-        iPlaySound("wrong.wav", false, 80);
-        iResumeSound(easy);
-        isObsActive[i] = false; 
-    }
-    }
-  }
+                    if (health <= 0) {
+                        finalScore = score;
+                        addHighScore(playerName, finalScore);
+                        gamestate = 6;  // Switch to Game Over screen
+                    } else {
+                        iResumeSound(easy);
+                    }
+                }
+            }
+        }
 
-if(showHeartbreak)
-{
-    iShowLoadedImage(200, 400, &heartbreak);
-    heartbreakCounter--;
-    if(heartbreakCounter <= 0)
-    {
-        showHeartbreak = false;
+        if (showHeartbreak) {
+            iShowLoadedImage(200, 400, &heartbreak);
+            if (--heartbreakCounter <= 0) 
+                showHeartbreak = false;
+        }
+
+        char str[30];
+        sprintf(str, "Score: %d", score);
+        iText(10, 760, str, GLUT_BITMAP_HELVETICA_18);
+        sprintf(str, "Speed: %.2f", obstacleSpeed);
+        iText(10, 740, str, GLUT_BITMAP_HELVETICA_18);
+        char lifeStr[20];
+        sprintf(lifeStr, "Lives: %d", health);
+        iText(10, 720, lifeStr, GLUT_BITMAP_HELVETICA_18);
+    }
+    else if (gamestate == 3) {
+        // Name input screen
+        iSetColor(255, 255, 255);
+        iText(100, 400, "Enter Your Name:", GLUT_BITMAP_HELVETICA_18);
+        iText(100, 370, playerName, GLUT_BITMAP_HELVETICA_18);
+        iText(100, 300, "Press ENTER to start the game.", GLUT_BITMAP_HELVETICA_18);
+    }
+    else if (gamestate == 4) {
+        // About screen
+        iShowImage(0, 0, "aboutus.PNG");
+    }
+    else if (gamestate == 6) {
+        // Game Over screen
+        iText(150, 450, "Game Over", GLUT_BITMAP_HELVETICA_18);
+        char finalScoreStr[50];
+        sprintf(finalScoreStr, "Final Score: %d", finalScore);
+        iText(150, 420, finalScoreStr, GLUT_BITMAP_HELVETICA_18);
+
+        // Show Back button so player can return to main menu
+        iShowLoadedImage(420, 740, &back);
+    }
+    else if (gamestate == 2) {
+    iSetColor(255, 255, 255);
+    iText(180, 700, "High Scores", GLUT_BITMAP_HELVETICA_18);
+    int startY = 650;
+    for (size_t i = 0; i < highScores.size(); i++) {
+        char scoreLine[100];
+        sprintf(scoreLine, "%d. %s - %d", (int)(i+1), highScores[i].name.c_str(), highScores[i].score);
+        iText(150, startY - i * 30, scoreLine, GLUT_BITMAP_HELVETICA_18);
+    }
+    // Back button to return to main menu
+    iShowLoadedImage(420, 740, &back);
+}
+
+    // Show Back button on other screens except main menu and game over (which has own back button)
+    if (gamestate > 0 && gamestate <= 5 && gamestate != 6) {
+        iShowLoadedImage(420, 740, &back);
     }
 }
-char str[30];
-sprintf(str,"Score: %d",score);
-iText(10,760,str,GLUT_BITMAP_HELVETICA_18);
-    }
-}
+
+
 
 /*
 function iMouseMove() is called when the user moves the mouse.
@@ -231,7 +299,10 @@ void iMouse(int button, int state, int mx, int my)
         {
             if(mx >= 110 && mx <= 220 && my >=310 && my <= 462) // Start Game
             {
-                gamestate = 1;
+               gamestate = 3;
+               inputName = true;
+              playerName[0] = '\0';
+               playerNameIndex = 0;
                 iResumeSound(easy);
             }
             else if(mx >= 290 && mx <=420 && my >=310 && my <=470) // Instructions
@@ -241,7 +312,7 @@ void iMouse(int button, int state, int mx, int my)
             }
             else if(mx >= 30 && mx <= 140 && my >= 60 && my <= 210) // Exit
             {
-                gamestate = 3;
+                 
             }
             else if(mx >= 195 && mx <= 310 && my >= 60 && my <=220) // About
             {
@@ -249,9 +320,18 @@ void iMouse(int button, int state, int mx, int my)
             }
             else if(mx >= 360 && mx <= 480 && my >= 60 && my <= 210) // Credits
             {
-                gamestate = 5;
+                exit(0);
             }
 
+        }
+        if (gamestate >= 1 && gamestate <= 6)
+    {
+            if(mx >= 420 && mx <= 500 && my >= 740 && my <= 800) // Back button
+            {
+                gamestate = 0;
+                iPauseSound(easy);
+                iResumeSound(easy);
+            }
         }
             
     }
@@ -267,15 +347,39 @@ void iMouseWheel(int dir, int mx, int my)
 }
 void iKeyboard(unsigned char key)
 {
-    switch (key)
-    {
-    case 'q':
-        // do something with 'q'
-        break;
-    // place your codes for other keys here
-    default:
-        break;
-    }  
+    if (gamestate == 3 && inputName) {
+    if (key == '\b') { // backspace
+        if (playerNameIndex > 0) {
+            playerNameIndex--;
+            playerName[playerNameIndex] = '\0';
+        }
+    } else if (key == '\r' || key == '\n') { // enter
+        inputName = false;
+        gamestate = 1;
+        easy = iPlaySound("Easy.wav", true, 80);
+
+        // Reset game values
+        score = 0;
+        scoretick = 0;
+        health = 3;
+        obstacleSpeed = 2.0;
+
+        for (int i = 0; i < 3; i++) {
+            obsy[i] = 800 + i * 267;
+            obsx[i] = rand() % 300;
+            obsi[i] = rand() % 3;
+            isObsActive[i] = true;
+            iChangeSpriteFrames(&obsimg[i], &obsimgfrm[obsi[i]], 1);
+            iSetSpritePosition(&obsimg[i], obsx[i], obsy[i]);
+        }
+    } else if (playerNameIndex < sizeof(playerName) - 1) {
+        if (isalnum(key) || key == ' ') {
+            playerName[playerNameIndex++] = key;
+            playerName[playerNameIndex] = '\0';
+        }
+    }
+}
+
 }
 void iSpecialKeyboard(unsigned char key)
 {
@@ -292,6 +396,7 @@ int main(int argc, char *argv[])
     iInitSprite(&habimg[0]);
     iChangeSpriteFrames(&habimg[0], &habimgfrm[0], 1);
     iSetSpritePosition(&habimg[0], x, y);
+    loadHighScores();
 
     for (int i = 0; i < 3; i++) {
         iInitSprite(&obsimg[i]);
@@ -317,6 +422,7 @@ int main(int argc, char *argv[])
     iLoadImage(&menuImghab, "menuhab.png");
     iLoadImage(&skyImg, "sky.jpg");
     iLoadImage(&heartbreak, "heartbreak.png");
+    iLoadImage(&back, "backbutton.png");
     iSetTimer(500,incmnt);
     iSetTimer(16,cldx);
     iSetTimer(16,cldy);
@@ -330,4 +436,6 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
+
 
